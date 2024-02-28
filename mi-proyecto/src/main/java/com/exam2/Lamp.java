63aa4f3eb8a571c8a41f60e23b1d8f40ff4f6234
Andreu Alfonso <andalfmeg@alu.edu.gva.es>
2024-02-28 12:37:39 +0100

    Start

---
+++ mi-proyecto/src/main/java/com/exam2/Lamp.java
@@ -0,0 +1,30 @@
+package com.exam2;
+
+public class Lamp {
+    public static void main(String[] args) {
+
+        public boolean isOn() {
+
+        }
+
+        public double getConsumption() {
+
+        }
+
+        public turnOn() {
+
+        }
+
+        public turnOff() {
+
+        }
+
+        public toggle() {
+
+        }
+
+        public double consume(double seconds) {
+            
+        }
+    }
+}
---
+++ mi-proyecto/src/test/java/com/exam2/LampTest.java
@@ -0,0 +1,17 @@
+package com.exam2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class LampTest {
+
+    @Test
+    @DisplayName("Class Exists")
+    public void Lamp() {
+
+    }
+
+    @Nested
+    @DisplayName("Nested ")
+}
